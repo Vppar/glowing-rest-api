ed0a7d6ad197d9e51a0742897c930ca4fe1315fd
Mathias Kretschek <mathias@kretschek.com.br>
2014-05-23 17:02:37 -0300

    Implemented a quick test for the GET /core/session endpoint

--- test/routes/session.js
+++ test/routes/session.js
@@ -25,7 +25,6 @@ describe('session routes', function () {
         .set('Content-Type', 'application/json')
         .send(credentials)
         .expect(function (res) {
-          console.log(res);
           if (res.statusCode === HttpStatus.NOT_FOUND) {
             throw new Error('POST /core/session is not defined');
           }
@@ -36,5 +35,20 @@ describe('session routes', function () {
 
 
   describe('GET /core/session', function () {
+    it('is defined', function (done) {
+      var token = '0e1d384e442ecb9f43284f5b51eb3f94c84a6bbc496592f37fa4f153061e8bdb';
+
+      request(app)
+        .get('/core/session')
+        .set('Content-Type', 'application/json')
+        .query({token : token})
+        .expect(function (res) {
+          console.log(res);
+          if (res.statusCode === HttpStatus.NOT_FOUND) {
+            throw new Error('GET /core/session is not defined');
+          }
+        })
+        .end(done);
+    });
   });
 });
