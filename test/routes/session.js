7fb7d390d693d73b15f6593c35b62bc15231d905
Mathias Kretschek <mathias@kretschek.com.br>
2014-05-26 14:05:18 -0300

    SessionEndpoint.end() initial implementation

--- test/routes/session.js
+++ test/routes/session.js
@@ -25,6 +25,7 @@ describe('session routes', function () {
         .set('Content-Type', 'application/json')
         .send(credentials)
         .expect(function (res) {
+          console.log('>>> status (POST)', res.statusCode);
           if (res.statusCode === HttpStatus.NOT_FOUND) {
             throw new Error('POST /core/session is not defined');
           }
@@ -43,9 +44,29 @@ describe('session routes', function () {
         .set('Content-Type', 'application/json')
         .query({token : token})
         .expect(function (res) {
-          console.log(res);
+          console.log('>>> status (GET)', res.statusCode);
           if (res.statusCode === HttpStatus.NOT_FOUND) {
             throw new Error('GET /core/session is not defined');
+          }
+        })
+        .end(done);
+    });
+  });
+
+
+  describe.only('DELETE /core/session', function () {
+    it('is defined', function (done) {
+      this.timeout(5000);
+
+      var token = '0e1d384e442ecb9f43284f5b51eb3f94c84a6bbc496592f37fa4f153061e8bdb';
+
+      request(app)
+        .del('/core/session')
+        .query({token : token})
+        .expect(function (res) {
+          console.log('>>> status (DELETE)', res.statusCode);
+          if (res.statusCode === HttpStatus.NOT_FOUND) {
+            throw new Error('DELETE /core/session is not defined');
           }
         })
         .end(done);
