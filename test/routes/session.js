872e012b8eefb9c0539772a110c7bd3814e500d3
Mathias Kretschek <mathias@kretschek.com.br>
2014-05-26 16:37:06 -0300

    Implemented basic test for POST /core/session/refresh

--- test/routes/session.js
+++ test/routes/session.js
@@ -72,4 +72,26 @@ describe('session routes', function () {
         .end(done);
     });
   });
+
+
+  describe('POST /core/session/refresh', function () {
+    it('is defined', function (done) {
+      this.timeout(5000);
+
+      var token = '50c5bd92c57b01fdf40e62808b1c54641a614ed611d81614f7a997cfdc4f21b1';
+      var refresh = '2bcf2dd44304ede3a5e1ba87c8daf1c3a8f668b7570bd67825e8e86a6234b78c';
+
+      request(app)
+        .post('/core/session/refresh')
+        .query({token : token})
+        .send({refresh : refresh})
+        .expect(function (res) {
+          console.log('>>> status (POST /refresh)', res.statusCode, res.body);
+          if (res.statusCode === HttpStatus.NOT_FOUND) {
+            throw new Error('POST /core/session/refresh is not defined');
+          }
+        })
+        .end(done);
+    });
+  });
 });
